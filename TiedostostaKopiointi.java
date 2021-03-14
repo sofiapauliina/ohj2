a082a77b582728c8224766e449cd0b4713dfeca0
stiitin <46709033+sofiapauliina@users.noreply.github.com>
2021-03-14 12:29:35 +0200

    Update TiedostostaKopiointi.java

--- TiedostostaKopiointi.java
+++ TiedostostaKopiointi.java
@@ -1,3 +1,21 @@
+/* Ohjelma, joka lukee tiedostoa, jossa on välilyönnein toisistaan erotettuja lukuja ja sanoja, 
+* ja kopioi toiseen tiedostoon ne rivit, joiden alussa on luku, joka on suurempi kuin 30. 
+* Luettavan tiedoston nimi ja tulostustiedoston nimi annetaan ohjelman args-parametreissä.
+*
+* Esimerkiksi tiedostosta:
+*
+* 33 hiljaa 1 hiipii
+* hyvä 33 tulee
+* 25 jep
+* 36 1 3 5 55
+* nyt 33 riittää
+*
+* tulee tiedosto:
+*
+* 33 hiljaa 1 hiipii
+* 36 1 3 5 55
+*/    
+    
 package demo.d9;
 import java.io.*; 
 import java.util.*; 
